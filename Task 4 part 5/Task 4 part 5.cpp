705ccd45e1f978802bca64f8b1d09ac9ff520200
Limonchik-D <DavLimon4@yandex.com>
2025-06-12 10:01:54 +0300

    Update Task 4 part 5.cpp

--- Task 4 part 5/Task 4 part 5.cpp
+++ Task 4 part 5/Task 4 part 5.cpp
@@ -5,51 +5,12 @@
 using namespace std;
 
 double computeExpression(int n) {
-    double sum_sin = 0.0;
-    double term1 = 0.0;
-    double term2 = 0.0;
-    double term3 = 0.0;
-    double one = 1.0;
-    double neg_one = -1.0;
-
-    // Считаем сумму sin(i)
-    for (int i = 1; i <= n; i++) {
-        sum_sin += sin(i);
-    }
-
-    // sin(1) и sin(1)+sin(2)
-    double s1 = sin(1.0);
-    double s12 = sin(1.0) + sin(2.0);
-
-    __asm {
-        finit
-
-        // term1 = 1 / sin(1)
-        fld     one
-        fld     s1
-        fdivp   st(1), st
-        fstp    term1
-
-        // term2 = 1 / (sin(1) + sin(2))
-        fld     one
-        fld     s12
-        fdivp   st(1), st
-        fstp    term2
-
-        // term3 = -1 / sum_sin
-        fld     neg_one
-        fld     sum_sin
-        fdivp   st(1), st
-        fstp    term3
-    }
-
     double result = 0.0;
-    __asm {
-        finit
-        fld     term1
-        fadd    term2
-        fsub    term3
-        fstp    result
+    double product = 1.0; // Хранит произведение sin(1)*sin(2)*...*sin(k)
+
+    for (int k = 1; k <= n; k++) {
+        product *= sin(k);
+        result += 1.0 / product;
     }
 
     return result;
@@ -60,10 +21,10 @@ int main() {
     SetConsoleOutputCP(1251);
 
     int n;
-    cout << "Введите натуральное число n (n >= 2): ";
+    cout << "Введите натуральное число n (n >= 1): ";
     cin >> n;
-    if (n < 2) {
-        cout << "Ошибка: n должно быть не менее 2." << endl;
+    if (n < 1) {
+        cout << "Ошибка: n должно быть не менее 1." << endl;
         return 1;
     }
 
