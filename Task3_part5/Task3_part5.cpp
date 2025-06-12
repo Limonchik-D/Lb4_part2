b689550fe106c03d80c3a8ee1d853873ea106bc0
Limonchik-D <DavLimon4@yandex.com>
2025-06-12 09:32:49 +0300

    Update Task3_part5.cpp

--- Task3_part5/Task3_part5.cpp
+++ Task3_part5/Task3_part5.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 #include <conio.h>
 
@@ -11,45 +11,44 @@ int main() {
     int k = 1;
 
     __asm {
-        finit           // Инициализация сопроцессора
+        finit           // Инициализация FPU
         fldz            // ST(0) = 0.0 — начальная сумма
 
         mov k, 1
 
-        loop_start:
+    loop_start:
         cmp k, 6
-            jge end_loop
+        jge end_loop
 
-            // Вычисляем степень x^{2k}
-            fld x          // ST(0) = x
-            fmul st(0), st(0)    // x^2
-            mov ecx, 1
+        // Вычисляем x^(2k)
+        fld x              // ST(0) = x
+        fmul st(0), st(0)  // ST(0) = x^2
 
-            power_loop:
+        mov ecx, 1
+
+    power_loop:
         inc ecx
-            cmp ecx, k
-            jg end_power
+        cmp ecx, k
+        jg end_power
+        fmul st(0), st(0)  // ST(0) = x^(2*ecx)
+        jmp power_loop
 
-            fmul st(0), st(0)    // Умножаем ещё раз: x^4, x^6 и т.д.
-            jmp power_loop
-
-            end_power :
-
-        // Делим на (2k)
+    end_power:
+        // Делим на 2k
         mov eax, k
-            shl eax, 1           // 2k
-            push eax
-            fild dword ptr[esp] // ST(0) = 2k
-            add esp, 4
-            fdiv                // ST(0) = x^{2k} / 2k
+        shl eax, 1           // eax = 2k
+        push eax
+        fild dword ptr [esp] // ST(0) = 2k (int -> float)
+        add esp, 4
+        fdiv                // ST(0) = x^(2k) / 2k
 
-            fadd                // Сумма += результат
+        fadd                // sum += ...
 
-            inc k
-            jmp loop_start
+        inc k
+        jmp loop_start
 
-            end_loop :
-        fstp sum
+    end_loop:
+        fstp sum            // sum = ST(0)
     }
 
     std::cout << "Сумма ряда при x = -1.1: " << sum << "\n";
